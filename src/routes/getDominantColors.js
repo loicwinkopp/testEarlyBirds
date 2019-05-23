f22c5b9c5830a4683e01bcc4e370df79bdf0e369
Loic <loicwinkopp@gmail.com>
2019-05-23 02:11:02 +0200

    Finalisation
    Ajout de commentaires
    Suppression de l'image dans le bucket après son utilisation
    Completion de la liste précalculée des produits et des couleurs dominantes
    Ajouts de réponses d'erreur

--- src/routes/getDominantColors.js
+++ src/routes/getDominantColors.js
@@ -9,6 +9,12 @@ const bucketName = 'images-eb75';
 const bucket = storage.bucket(bucketName);
 
 module.exports = async function getDominantColors(products, res, index) {
+  if (products.length === 0) {
+    res.status(400).json('No product');
+    return;
+  }
+
+  // Si la liste de produits à été balayée en entier, renvoyer la liste des produits avec les couleurs dominantes
   if (index > products.length - 1) {
     res.status(200).json(products);
     return;
@@ -17,7 +23,7 @@ module.exports = async function getDominantColors(products, res, index) {
   const i = index || 0;
   const product = products[i];
 
-  // Generation du nom de l'image dans le bucket
+  // Géneration du nom de l'image dans le bucket
   const fileName = 'image' + Math.floor(Date.now() / 1000) + '.jpg';
 
   // Writestream de l'image
@@ -25,21 +31,24 @@ module.exports = async function getDominantColors(products, res, index) {
   const writeStream = file.createWriteStream();
   const imageURL = 'http:' + product.photo;
 
-  // Ecriture de l'image dans le bucket
+  // Téléchargement et écriture de l'image dans le bucket
   const fetchRes = await fetch(imageURL);
   fetchRes.body.pipe(writeStream);
 
   // Lorsque le writeStream est termine...
   writeStream.on('finish', async () => {
-    // Appel a l'api Vision
+    // Appel a l'API Vision
     const [result] = await client.imageProperties(`gs://${bucketName}/${fileName}`);
+
+    // Suppression de l'image du bucket
+    await bucket.file(fileName).delete();
 
     const colors = result.imagePropertiesAnnotation.dominantColors.colors;
 
     // Ajout de l'attribut dominantColor au produit
     product.dominantColor = colors[0].color;
 
-    // Recursion lorsque le writeStream est termine
+    // Recursion lorsque le writestream est terminé
     getDominantColors(products, res, i + 1);
   });
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(saveProducts, getProducts) {
   const express = require('express');
+  const fs = require('fs');
   const csvToJson = require('convert-csv-to-json');
   const getDominantColors = require('./getDominantColors');
   const getRecommendations = require('./getRecommendations');
@@ -7,20 +8,24 @@ module.exports = function(saveProducts, getProducts) {
   const router = express.Router();
 
   // Création d'une liste de produits
-  router.post('/products', async function(req, res) {
+  router.post('/products', function(req, res) {
+    if (!fs.existsSync(req.query.csv)) {
+      res.status(400).send('File does not exist');
+      return;
+    }
     saveProducts(csvToJson.getJsonFromCsv(req.query.csv));
     res.status(200).send('Product list received');
   });
 
   // Ajoute les couleurs dominantes à la liste de produits et renvoie cette liste
-  router.get('/dominantcolors', async function(req, res) {
+  router.get('/dominantcolors', function(req, res) {
     const products = getProducts();
 
     getDominantColors(products, res);
   });
 
   // Renvoie les 5 produits dont la couleur dominante est la plus proche de celle du produit dont l'ID est passé en paramètre
-  router.get('/recommendations', async function(req, res) {
+  router.get('/recommendations', function(req, res) {
     getRecommendations(req, res);
   });
 
